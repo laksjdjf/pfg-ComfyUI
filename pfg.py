16aea0ce0b6480ac794adca8e3f736194a446f63
laksjdjf <aikasama@hotmail.co.jp>
2023-07-11 20:35:48 +0900

    a

---
+++ pfg.py
@@ -0,0 +1,89 @@
+import os
+import torch
+import numpy as np
+from PIL import Image
+from .pfg_model import ViT
+from .pfg_utils import download, preprocess_image, TAGGER_FILE
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def get_file_list(path):
+    return [file for file in os.listdir(path) if file != "put_models_here.txt"]
+
+class PFG:
+    def __init__(self):
+        download(CURRENT_DIR)
+        self.tagger = ViT(3, 448, 9083)
+        self.tagger.load_state_dict(torch.load(os.path.join(CURRENT_DIR, TAGGER_FILE)))
+        self.tagger.eval()
+
+    # wd-14-taggerの推論関数
+    @torch.no_grad()
+    def infer(self, img: Image):
+        img = preprocess_image(img)
+        img = torch.tensor(img).permute(2, 0, 1).unsqueeze(0)
+        print("inferencing by torch model.")
+        probs = self.tagger(img).squeeze(0)
+        return probs
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "positive": ("CONDITIONING",),
+                "negative": ("CONDITIONING",),
+                "pfg_scale": ("FLOAT", {
+                    "default": 1, 
+                    "min": 0, #Minimum value
+                    "max": 2, #Maximum value
+                    "step": 0.05 #Slider's step
+                }),
+                "attention_dim":  ((768, 1024, 2048), ),
+                "image": ("IMAGE", ), 
+                "model_name": (get_file_list(os.path.join(CURRENT_DIR,"models")), ),
+            }
+        }
+    RETURN_TYPES = ("CONDITIONING", "CONDITIONING")
+    FUNCTION = "add_pfg"
+    CATEGORY = "loaders"
+
+    def add_pfg(self, positive, negative, pfg_scale, image, attention_dim, model_name):
+        # load weight
+        pfg_weight = torch.load(os.path.join(CURRENT_DIR, "models/" + model_name))
+        weight = pfg_weight["pfg_linear.weight"].cpu()
+        bias = pfg_weight["pfg_linear.bias"].cpu()
+        
+        # comfyのload imageはtensorを返すので一度pillowに戻す
+        tensor = image*255
+        tensor = np.array(tensor, dtype=np.uint8)
+        image = Image.fromarray(tensor[0])
+        
+        # tagger特徴量の計算
+        pfg_feature = self.infer(image)
+        
+        # pfgの計算
+        pfg_cond = (weight @ pfg_feature + bias) * pfg_scale
+        pfg_cond = pfg_cond.reshape(1, -1, attention_dim)
+        
+        # text_embs
+        cond = positive[0][0]
+        uncond = negative[0][0]
+
+        # cond側
+        pfg_cond = pfg_cond.to(cond.device, dtype=cond.dtype)
+        pfg_cond = pfg_cond.repeat(cond.shape[0], 1, 1)
+
+        # uncond側はゼロベクトルでパディング
+        cond = torch.cat([cond, pfg_cond], dim=1)
+        pfg_uncond_zero = torch.zeros(uncond.shape[0], pfg_cond.shape[1], uncond.shape[2]).to(uncond.device, dtype=uncond.dtype)
+        uncond = torch.cat([uncond, pfg_uncond_zero], dim=1)
+
+        return ([[cond, positive[0][1]]], [[uncond, negative[0][1]]])
+        
+NODE_CLASS_MAPPINGS = {
+    "PFG": PFG
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PFG": "Load PFG node",
+}
---
+++ pfg_utils.py
@@ -0,0 +1,33 @@
+#このコードはhttps://github.com/kohya-ss/sd-scripts/blob/main/finetune/tag_images_by_wd14_tagger.pyを参考にしていますというかパクっています。
+
+from huggingface_hub import hf_hub_download
+import cv2
+import numpy as np
+import os
+IMAGE_SIZE = 448
+
+TAGGER_REPO = "furusu/wd-v1-4-tagger-pytorch"
+TAGGER_FILE = "wd-v1-4-vit-tagger-v2.ckpt"
+
+def download(path):
+    if not os.path.exists(os.path.join(path, TAGGER_FILE)):
+        hf_hub_download(TAGGER_REPO, TAGGER_FILE, cache_dir=path, force_download=True, force_filename=TAGGER_FILE)
+
+def preprocess_image(image):
+    image = image.convert("RGB")
+    image = np.array(image)
+    image = image[:, :, ::-1]  # RGB->BGR
+
+    # pad to square
+    size = max(image.shape[0:2])
+    pad_x = size - image.shape[1]
+    pad_y = size - image.shape[0]
+    pad_l = pad_x // 2
+    pad_t = pad_y // 2
+    image = np.pad(image, ((pad_t, pad_y - pad_t), (pad_l, pad_x - pad_l), (0, 0)), mode="constant", constant_values=255)
+
+    interp = cv2.INTER_AREA if size > IMAGE_SIZE else cv2.INTER_LANCZOS4
+    image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE), interpolation=interp)
+
+    image = image.astype(np.float32)
+    return image
